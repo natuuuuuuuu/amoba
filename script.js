dd2a087b90db50959a400357d8d08d789e827867
natuuuuuuuu <dulganov.n@gmail.com>
2024-05-11 19:27:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     let gameEnded = false;
 
     // Játék céljának üzenete
-    alert("Üdvözöllek az Amőba játékban! A célod három saját jelölés egymás melletti elhelyezése sorban, oszlopban vagy átlósan.");
+   // Játék céljának üzenete
+alert("Üdvözöllek az Amőba játékban!\n\nA játék két játékos által játszható. Az első játékos az 'X' jelzéssel kezd, majd váltakozva helyeznek el jelzéseket a mezőkben. A célod három saját jelölés egymás melletti elhelyezése sorban, oszlopban vagy átlósan. Aki először ér el ebben célban három saját jelzést, az nyer a játékban. Sok szerencsét!");
+
+
 
     cells.forEach(cell => {
         cell.addEventListener("click", function() {
